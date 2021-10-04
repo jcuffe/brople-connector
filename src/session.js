ed013129d082edd186d791e2164240be27038d39
Jared Cuffe <jcuffe@gmail.com>
2021-10-03 19:59:24 -0700

    Loads session from database.

--- src/session.js
+++ src/session.js
@@ -1,12 +1,15 @@
 import { getProducts } from './utils/cartStore'
+import getClient from "./database/client"
 
 export default async function session(req, res) {
+  const client = getClient()
+  const session = await client("sessions").select("*").first()
   return {
-    name: 'Mark',
-    email: 'mark@domain.com',
+    name: session.name,
+    email: session.email,
     cart: {
       items: getProducts(req, res),
     },
-    currency: 'USD',
+    currency: session.currency,
   }
 }
