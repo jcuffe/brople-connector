d29febce8495db76e645ebfd2430f96fe493977c
Jared Cuffe <jcuffe@gmail.com>
2021-10-05 11:02:03 -0700

    Updates handlers.

--- src/session.js
+++ src/session.js
@@ -1,15 +1,19 @@
-import { getProducts } from './utils/cartStore'
-import getClient from "./database/client"
+import { getProducts } from "./utils/cartStore"
+import Session from "./database/models/session"
+import withTransaction from "./database/client"
 
-export default async function session(req, res) {
-  const client = getClient()
-  const session = await client("sessions").select("*").first()
-  return {
-    name: session.name,
-    email: session.email,
-    cart: {
-      items: getProducts(req, res),
-    },
-    currency: session.currency,
+function sessionHandlerFactory(req, res) {
+  return async function sessionHandler(trx) {
+    const session = await Session.query(trx).first()
+    return {
+      name: session.name,
+      email: session.email,
+      cart: {
+        items: getProducts(req, res),
+      },
+      currency: session.currency,
+    }
   }
 }
+
+export default withTransaction(sessionHandlerFactory)
