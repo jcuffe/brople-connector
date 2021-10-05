d624d56e52352f9743a40f30cb545281b21a3e69
Jared Cuffe <jcuffe@gmail.com>
2021-10-05 11:01:20 -0700

    Adds ORM init.

--- src/database/client.js
+++ src/database/client.js
@@ -1,14 +1,23 @@
+import { Model, knexSnakeCaseMappers } from "objection"
 import Knex from "knex"
 import knexfile from "../../knexfile"
 
-let client
+export default function withTransaction(handlerFactory) {
+  return async function handleRequest(...handlerArgs) {
+    // Merge column mapping into config
+    const knexConfig = {
+      ...knexfile[process.env.ENVIRONMENT],
+      ...knexSnakeCaseMappers(),
+    }
 
-export default function getClient() {
-  if (!client) {
-    const config = knexfile[process.env.ENVIRONMENT]
-    console.log(config)
-    client = Knex(config)
+    // Prepare the database connection
+    const knex = Knex(knexConfig)
+    Model.knex(knex)
+
+    // Wrap request handler logic in a transaction
+    const requestHandler = handlerFactory(...handlerArgs)
+    return Model.transaction(requestHandler)
+      .catch((err) => console.log(err))
+      .finally(() => knex.destroy())
   }
-
-  return client
 }
